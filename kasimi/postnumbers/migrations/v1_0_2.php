b0bdab04562a99ede1fad7c3dfdca4040cb15ec4
kasimi <kasimi@inbox.ru>
2015-12-12 21:26:00 +0100

    Version bump 1.0.2

---
+++ kasimi/postnumbers/migrations/v1_0_2.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Post Numbers
+ * @copyright (c) 2015 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace kasimi\postnumbers\migrations;
+
+class v1_0_2 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\kasimi\postnumbers\migrations\v1_0_1');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.update', array('kasimi.postnumbers.version', '1.0.2')),
+		);
+	}
+}
