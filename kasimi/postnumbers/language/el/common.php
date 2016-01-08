9a5b2ed63a6c3153103bf0c3107aa57ff39b4c98
kasimi <kasimi@inbox.ru>
2016-01-08 12:23:28 +0100

    Added Greek translation by gtsoukn

---
+++ kasimi/postnumbers/language/el/common.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Post Numbers
+ * Ελληνική μετάφραση [el]
+ *
+ * @copyright (c) 2015 kasimi
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'POSTNUMBERS_COPY_TITLE'		=> 'Αντιγραφή συνδέσμου δημοσίευσης στο πρόχειρο',
+	'POSTNUMBERS_COPY_MANUALLY'		=> 'Αντιγραφή στο πρόχειρο: Ctrl/Cmd+C, Enter',
+	'POSTNUMBERS_COPIED'			=> 'Αντιγράφηκε!',
+));
