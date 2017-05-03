aa819d011f5329b525bb7c61cdeb71ebeaf35c3c
kasimi <mail@kasimi.net>
2017-05-03 12:36:30 +0200

    Improved compatibility with Quickedit extension

--- kasimi/postnumbers/styles/all/template/postnumbers.js
+++ kasimi/postnumbers/styles/all/template/postnumbers.js
@@ -82,4 +82,23 @@ jQuery(function($) {
 			}
 		});
 	});
+
+	/**
+	 * Compatibility with the Quickedit extension by Marc
+	 *
+	 * When clicking on a post's Edit icon, the Quickedit extension adds the text
+	 * editor after the .author element. Since the Post Numbers extension adds
+	 * an element that also has this .author CSS class, the editor is added twice.
+	 * This is why we remove the .author CSS class before the Quickedit extension
+	 * adds the editor, and add it back afterwards.
+	 */
+	var quickEdit = phpbb.ajaxCallbacks['quickedit_post'];
+	if (quickEdit) {
+		phpbb.addAjaxCallback('quickedit_post', function(res) {
+			var $postNumber = $('#p' + res.POST_ID).find('p.post-number');
+			$postNumber.removeClass('author');
+			quickEdit.apply(this, arguments);
+			$postNumber.addClass('author');
+		});
+	}
 });
