66b12155788eeb9ef94a047156d2b21742002c0d
Jan Pogocki <jan.pogocki@gmail.com>
2017-07-24 16:21:07 +0200

    polish language

---
+++ kasimi/postnumbers/language/pl/acp.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Post Numbers
+ * @copyright (c) 2016 kasimi - https://kasimi.net
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'POSTNUMBERS_ENABLED_VIEWTOPIC'			=> 'Pokaż numerację postów podczas wyświetlania tematów',
+	'POSTNUMBERS_ENABLED_REVIEW_REPLY'		=> 'Pokaż numerację postów (w przeglądzie tematu) podczas pisania odpowiedzi',
+	'POSTNUMBERS_ENABLED_REVIEW_MCP'		=> 'Pokaż numerację postów w Panelu Moderacji',
+	'POSTNUMBERS_SKIP_NONAPPROVED'			=> 'Opóść niezaakceptowane posty',
+	'POSTNUMBERS_SKIP_NONAPPROVED_EXP'		=> 'Nie przypisuj numeracji postów do niezaakceptowanych i ukrytych postów',
+	'POSTNUMBERS_DISPLAY_IDS'				=> 'Wyświetlaj globalne ID postów zamiast numeracji w ramach tematu',
+	'POSTNUMBERS_DISPLAY_IDS_EXP'			=> 'Jeśli opcja jest włączona, powyższa opcja <span style="font-style: italic;">Opóść niezaakceptowane posty</span> będzie ignorowana',
+	'POSTNUMBERS_LOCATION'					=> 'Lokalizacja numeracji postów',
+	'POSTNUMBERS_LOCATION_AUTHOR'			=> 'Przed ikoną postu i nazwą użytkownika autora postu',
+	'POSTNUMBERS_LOCATION_SUBJECT'			=> 'W temacie postu',
+	'POSTNUMBERS_CLIPBOARD'					=> 'Kopiuj link do posta po kliknięciu w numerację',
+	'POSTNUMBERS_CLIPBOARD_EXP'				=> 'Opcja ma znaczenie tylko wtedy, gdy numeracja postu znajduje się przed ikoną postu i nazwą użytkownika autora postu. Działa wyłącznie z nowoczesnymi przeglądarkami, w wersjach co najmniej: Chrome 43, Firefox 41, Internet Explorer 9, Opera 29, Safari 10. W niewspieranych przeglądarkach będzie wyświetlany komunikat z linkiem do postu, zamiast akcji kopiowania.',
+	'POSTNUMBERS_BOLD'						=> 'Wyświetlaj numerację postów/globalne ID pogrubioną czcionką',
+	'POSTNUMBERS_BOLD_EXP'					=> 'Opcja ma znaczenie tylko wtedy, gdy gdy numeracja postu znajduje się przed ikoną postu i nazwą użytkownika autora postu.',
+));
---
+++ kasimi/postnumbers/language/pl/common.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Post Numbers
+ * @copyright (c) 2016 kasimi - https://kasimi.net
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'POSTNUMBERS_COPY_TITLE'		=> 'Kopiuj link posta do schowka',
+	'POSTNUMBERS_COPY_MANUALLY'		=> 'Kopiuj do schowka: Ctrl/Cmd+C, Enter',
+	'POSTNUMBERS_COPIED'			=> 'Skopiowano link!',
+));
