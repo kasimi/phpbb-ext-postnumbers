121216932de233dd6b0e36fc24c80c13cfa2ed4d
Jan Pogocki <janpogocki@users.noreply.github.com>
2017-07-27 10:20:04 +0200

    polish language (#11)

---
+++ kasimi/postnumbers/language/pl/info_acp_postnumbers.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - Post Numbers
+ * @copyright (c) 2016 kasimi - https://kasimi.net
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'POSTNUMBERS_TITLE'						=> 'Post Numbers',
+	'POSTNUMBERS_CONFIG'					=> 'Konfiguracja',
+	'POSTNUMBERS_CONFIG_UPDATED'			=> '<strong>Post Numbers </strong><br />» Konfiguracja zaktualizowana',
+));
